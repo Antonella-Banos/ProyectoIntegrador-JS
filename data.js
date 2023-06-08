abe37cdfd98ed2df1cde8975ded105293894bc72
Antonella <antonella.b95@hotmail.com>
2023-06-08 15:53:38 -0300

    categories

--- data.js
+++ data.js
@@ -1,44 +1,45 @@
 const = productsData [
     {
         id: 1,
-        name: "BLUSA ESTAMPADA VANESSA",
+        name: "BLUSA VANESSA",
         price: "$8000",
         category: "blusas",
         img: "./assets/img/products/product1.png",
     },
     {
         id: 2,
-        name: "TOP CROCHET ANGELA",
+        name: "TOP ANGELA",
         price: "$7000",
         category: "tops",
         img: "./assets/img/products/product2.png",
     },
     {
         id: 3,
-        name: "TOP ESTAMPADO MARINA",
+        name: "TOP MARINA",
         price: "$6500",
         category: "tops",
         img: "./assets/img/products/product3.png",
     },
     {
         id: 4,
-        name: "TOP ESTAMPADO ROSALIA",
+        name: "TOP ROSALIA",
         price: "$6500",
         category: "tops",
         img: "./assets/img/products/product4.png",
     },
     {
         id: 5, 
-        name: "JEANS ACAMPANADOS FAUSTINA",
+        name: "JEANS FAUSTINA",
         price: "$9000",
         category: "jeans",
         img: "./assets/img/products/product5.png",
     },
     {
         id: 6,
-        name: "VESTIDO CALADO ISABELLA",
+        name: "VESTIDO ISABELLA",
         price: "$10000",
         category: "vestidos",
         img: "./assets/img/products/product6.png",
     },
 ];
+
