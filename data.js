30a4e3351a834960e6333dce69e2bb7559ef9253
Antonella <antonella.b95@hotmail.com>
2023-06-09 18:18:36 -0300

    data

--- data.js
+++ data.js
@@ -1,45 +1,71 @@
+const boton = document.querySelector(".btn-buy");
+
+
 const = productsData [
     {
         id: 1,
-        name: "BLUSA VANESSA",
-        price: "$8000",
-        category: "blusas",
-        img: "./assets/img/products/product1.png",
+        prodName: "BLUSA VANESSA",
+        prodPrice: "$8000",
+        prodCategory: "blusas",
+        prodImg: "./assets/img/products/product1.png",
     },
     {
         id: 2,
-        name: "TOP ANGELA",
-        price: "$7000",
-        category: "tops",
-        img: "./assets/img/products/product2.png",
+        prodName: "TOP ANGELA",
+        prodPrice: "$7000",
+        prodCategory: "tops",
+        prodImg: "./assets/img/products/product2.png",
     },
     {
         id: 3,
-        name: "TOP MARINA",
-        price: "$6500",
-        category: "tops",
-        img: "./assets/img/products/product3.png",
+        prodName: "TOP MARINA",
+        prodPrice: "$6500",
+        prodCategory: "tops",
+        prodImg: "./assets/img/products/product3.png",
     },
     {
         id: 4,
-        name: "TOP ROSALIA",
-        price: "$6500",
-        category: "tops",
-        img: "./assets/img/products/product4.png",
+        prodName: "TOP ROSALIA",
+        prodPrice: "$6500",
+        prodCategory: "tops",
+        prodImg: "./assets/img/products/product4.png",
     },
     {
         id: 5, 
-        name: "JEANS FAUSTINA",
-        price: "$9000",
-        category: "jeans",
-        img: "./assets/img/products/product5.png",
+        prodName: "JEANS FAUSTINA",
+        prodPrice: "$9000",
+        prodCategory: "jeans",
+        prodImg: "./assets/img/products/product5.png",
     },
     {
         id: 6,
-        name: "VESTIDO ISABELLA",
-        price: "$10000",
-        category: "vestidos",
-        img: "./assets/img/products/product6.png",
+        prodName: "VESTIDO ISABELLA",
+        prodPrice: "$10000",
+        prodCategory: "vestidos",
+        prodImg: "./assets/img/products/product6.png",
     },
 ];
 
+const createBotonData = (botonData) => {
+    return `
+    <button 
+    class="btn-buy" 
+    data-id:"${botonData.id}"
+    data-name: "${botonData.prodName}"
+    data-price: "${botonData.prodPrice}"
+    data-img: "${botonData.prodImg}">COMPRAR</button>
+    `
+}
+
+const renderBoton = ( ) => {
+    boton.innerHTML = productsData.map((botonData) => createBotonData(botonData)).join("");
+
+}
+
+
+const init = ( ) => {
+    document.addEventListener("DOMContentLoaded", renderBoton);
+
+}
+
+init();
